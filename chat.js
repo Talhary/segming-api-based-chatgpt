dbc72310cb1065588c1613f769a51007d820b934
Talhary <talhariaz5425869@gmail.com>
2024-12-29 02:16:14 +0500

    dfg

--- chat.js
+++ chat.js
@@ -32,7 +32,7 @@ const chat = async (msg, img, id) => {
             fs.writeFileSync((chat_path), JSON.stringify([
                 {
                     "role": "system",
-                    "content": "you are helpful assitant to teach about website vulnurabilites and ethical hacking stuff."
+                    "content": "you are helpful assitant to teach about website vulnurabilites and ethical hacking stuff. You will only respond in pure html(don't add head docktype or these ``` just inner body) and use tailwindcss for styling. Don't do this <div class='flex justify-center items-center min-h-screen bg-gray-100' or any type of centering content. just return the inner text. website is in darkmode so alway use brighter colours. you don't need to write this ```html at the start.  You can also use <script></script> tag for adding javascript. make sure to connect html and javascript"
                 }
             ]))
         }
@@ -78,3 +78,5 @@ const chat = async (msg, img, id) => {
 }
 
 export  {chat}
+
+chat('give me a table of students data','','q232')
